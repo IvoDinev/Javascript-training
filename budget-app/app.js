0ffd5d849ea5433952419873c638087bf54efcc9
Ivaylo Dinev <201216004@fdiba.tu-sofia.bg>
2020-03-26 09:12:30 +0200

    getting of input data added

--- budget-app/app.js
+++ budget-app/app.js
@@ -3,12 +3,34 @@ var budgetController = (function() {
 })();
 
 var UIController = (function() {
-    //Some Code
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector('.add__type').value, //inc or exp
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+            };          
+        }
+    }
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
-    document.querySelector('.add__btn').addEventListener('click', function() {
-        console.log('clicked')
-    });
+    var ctrlAddItem = function() {
+        //1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
+        //2. Add the item to the budget controller
+        //3. Add the item to UI
+        //4. Calculate the budget
+        //5. Display the budget
+    }
+    
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    })
     
 })(budgetController, UIController);
